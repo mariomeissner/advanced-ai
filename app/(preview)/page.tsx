61dabb355e3762badd89f7456ba9f1122314d47a
Johannes Mario Meissner <jmariomeissner@gmail.com>
2024-12-31 11:26:23 +0900

    Add `cn` utility, empty state textbox vertically centered

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import { GitIcon, MasonryIcon, VercelIcon } from "@/components/icons";
 import Link from "next/link";
 import { useChat } from "ai/react";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   const { messages, handleSubmit, input, setInput, append } = useChat();
@@ -24,14 +25,14 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex flex-row justify-center pb-20 h-dvh bg-white dark:bg-zinc-900">
-      <div className="flex flex-col justify-between gap-4">
+    <div className="flex flex-row justify-center h-dvh bg-white dark:bg-zinc-900">
+      <div className={cn("flex flex-col w-full", messages.length === 0 ? 'justify-center' : 'justify-between pb-20')}>
         <div
           ref={messagesContainerRef}
-          className="flex flex-col gap-6 h-full w-dvw items-center overflow-y-scroll"
+          className={cn("flex flex-col gap-6 w-dvw items-center", messages.length > 0 ? 'h-full overflow-y-scroll' : '')}
         >
           {messages.length === 0 && (
-            <motion.div className="h-[350px] px-4 w-full md:w-[500px] md:px-0 pt-20">
+            <motion.div className="px-4 w-full md:w-[500px] md:px-0">
               <div className="border rounded-lg p-6 flex flex-col gap-4 text-zinc-500 text-sm dark:text-zinc-400 dark:border-zinc-700">
                 <p className="flex flex-row justify-center gap-4 items-center text-zinc-900 dark:text-zinc-50">
                   <VercelIcon size={16} />
@@ -74,48 +75,51 @@ export default function Home() {
           <div ref={messagesEndRef} />
         </div>
 
-        <div className="grid sm:grid-cols-1 gap-2 w-full px-4 md:px-0 mx-auto md:max-w-[500px] mb-4">
-          {messages.length === 0 &&
-            suggestedActions.map((suggestedAction, index) => (
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.05 * index }}
-                key={index}
-                className={index > 1 ? "hidden sm:block" : "block"}
-              >
-                <button
-                  onClick={async () => {
-                    append({
-                      role: "user",
-                      content: suggestedAction.action,
-                    });
-                  }}
-                  className="w-full text-left border border-zinc-200 dark:border-zinc-800 text-zinc-800 dark:text-zinc-300 rounded-lg p-2 text-sm hover:bg-zinc-100 dark:hover:bg-zinc-800 transition-colors flex flex-col"
-                >
-                  <span className="font-medium">{suggestedAction.title}</span>
-                  <span className="text-zinc-500 dark:text-zinc-400">
-                    {suggestedAction.label}
-                  </span>
-                </button>
-              </motion.div>
-            ))}
-        </div>
+        <div className="flex flex-col gap-4 items-center">
+          <form
+            className="flex flex-col gap-2 relative items-center w-full px-4 md:px-0"
+            onSubmit={handleSubmit}
+          >
+            <input
+              ref={inputRef}
+              className="bg-zinc-100 rounded-md px-2 py-1.5 w-full outline-none dark:bg-zinc-700 text-zinc-800 dark:text-zinc-300 md:max-w-[500px] max-w-[calc(100dvw-32px)]"
+              placeholder="Send a message..."
+              value={input}
+              onChange={(event) => {
+                setInput(event.target.value);
+              }}
+            />
+          </form>
 
-        <form
-          className="flex flex-col gap-2 relative items-center"
-          onSubmit={handleSubmit}
-        >
-          <input
-            ref={inputRef}
-            className="bg-zinc-100 rounded-md px-2 py-1.5 w-full outline-none dark:bg-zinc-700 text-zinc-800 dark:text-zinc-300 md:max-w-[500px] max-w-[calc(100dvw-32px)]"
-            placeholder="Send a message..."
-            value={input}
-            onChange={(event) => {
-              setInput(event.target.value);
-            }}
-          />
-        </form>
+          {messages.length === 0 && (
+            <div className="grid sm:grid-cols-1 gap-2 w-full px-4 md:px-0 mx-auto md:max-w-[500px]">
+              {suggestedActions.map((suggestedAction, index) => (
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.05 * index }}
+                  key={index}
+                  className={index > 1 ? "hidden sm:block" : "block"}
+                >
+                  <button
+                    onClick={async () => {
+                      append({
+                        role: "user",
+                        content: suggestedAction.action,
+                      });
+                    }}
+                    className="w-full text-left border border-zinc-200 dark:border-zinc-800 text-zinc-800 dark:text-zinc-300 rounded-lg p-2 text-sm hover:bg-zinc-100 dark:hover:bg-zinc-800 transition-colors flex flex-col"
+                  >
+                    <span className="font-medium">{suggestedAction.title}</span>
+                    <span className="text-zinc-500 dark:text-zinc-400">
+                      {suggestedAction.label}
+                    </span>
+                  </button>
+                </motion.div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
